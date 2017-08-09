f90faf69a94ff14d9d3f4ef6d6e17b19eabd21ef
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-09 18:37:25 -0500

    updated index to include math and expose individual fns on the origin

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var console = require('./console');
-var now = require('./now');
-var performance = require('./performance');
-var raf = require('./raf');
-module.exports = function polyfill(global) {
+module.exports = polyfill;
+var console = polyfill.console = require('./console');
+var now = polyfill.now = require('./now');
+var performance = polyfill.performance = require('./performance');
+var raf = polyfill.raf = require('./raf');
+var math = polyfill.math = require('./math');
+
+function polyfill(global) {
     console(global);
     now(global);
     performance(global);
     raf(global);
-};
+    math(global);
+}
