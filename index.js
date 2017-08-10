55e04c59417ea761edf40aea3d7b6523da51cb89
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-09 22:50:36 -0500

    added more sims

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ var now = polyfill.now = require('./now');
 var performance = polyfill.performance = require('./performance');
 var raf = polyfill.raf = require('./raf');
 var math = polyfill.math = require('./math');
+var registerElement = polyfill.registerElement = require('./register-element');
+var create = polyfill.create = require('./create');
+var bind = polyfill.bind = require('./bind');
+var matchMedia = polyfill.matchMedia = require('./match-media');
 
 function polyfill(global) {
+    bind(global);
     console(global);
     now(global);
     performance(global);
     raf(global);
     math(global);
+    registerElement(global);
+    create(global);
 }
