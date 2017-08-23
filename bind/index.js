96a39919311401c725b787e935394e1142c2670c
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-23 15:06:34 -0500

    adjusted performance to not set if one already exists to prevent hasownproperty from passing

--- bind/index.js
+++ bind/index.js
@@ -1,17 +1,19 @@
+var slice = [].slice;
+var PROTOTYPE = 'prototype';
 module.exports = function (global) {
     var Function = global.Function;
     if (!Function[PROTOTYPE].bind) {
-        Function[PROTOTYPE].bind = function (oThis) {
+        Function[PROTOTYPE].bind = function (context) {
             if (typeof this !== 'function') {
                 // closest thing possible to the ECMAScript 5
                 // internal IsCallable function
-                throw new TypeError('Function[PROTOTYPE].bind - what is trying to be bound is not callable');
+                throw new TypeError('Function.' + PROTOTYPE + '.bind - what is trying to be bound is not callable');
             }
-            var aArgs = Array[PROTOTYPE].slice.call(arguments, 1),
+            var aArgs = slice.call(arguments, 1),
                 fToBind = this,
                 FNOP = function () {},
                 fBound = function () {
-                    return fToBind.apply(this instanceof FNOP ? this : oThis, aArgs.concat(Array[PROTOTYPE].slice.call(arguments)));
+                    return fToBind.apply(this instanceof FNOP ? this : context, aArgs.concat(slice.call(arguments)));
                 };
             if (this[PROTOTYPE]) {
                 // native functions don't have a prototype
--- performance/index.js
+++ performance/index.js
@@ -2,7 +2,10 @@ module.exports = performance;
 var now = require('./now');
 function performance(global, adding) {
     var PERFORMANCE = 'performance';
-    var perf = global[PERFORMANCE] = global[PERFORMANCE] || {};
+    var perf = global[PERFORMANCE];
+    if (!perf) {
+        perf = global[PERFORMANCE] = {};
+    }
     if (!adding && !perf.now) {
         now(global);
     }
